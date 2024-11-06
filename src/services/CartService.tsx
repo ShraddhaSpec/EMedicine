6110607b906f0ed328c4658693cb5f986fcfb431
SPECINDIA\shraddha.parmar <shraddha.parmar@spec-india.com>
2024-11-06 18:31:34 +0530

    cart service

---
+++ src/services/CartService.tsx
@@ -0,0 +1,18 @@
+import api from '../API/api';
+
+export const CartService = {
+    getproducts ()  {
+        return api.get('/products/getproducts')
+            .then(response => {
+                return response.data.data;
+            })
+            .catch((error) => console.error('Error fetching data:', error))
+    },
+    getproductDetails(id : string | undefined) {
+        return api.get(`/products/getproduct/${id}`)
+            .then(response => {
+                return response.data.data;
+            })
+            .catch((error) => console.error('Error fetching data:', error))
+    }
+}
